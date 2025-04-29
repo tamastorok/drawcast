0d0686c753018fcfd1239cb2c62ab1811e209b5b
tamastorok <torok.tomi@gmail.com>
2025-04-29 14:34:45 +0200

    coiner text

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1331,7 +1331,7 @@ export default function Demo({ initialGameId }: { initialGameId?: string }) {
                           width={20} 
                           height={20} 
                           className="rounded-full transform rotate-[-2deg] cursor-help"
-                          title="Coiner"
+                          title="Coined drawing on the Collect page!"
                         />
                         <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xs rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                           Coined drawing on the Collect page!
